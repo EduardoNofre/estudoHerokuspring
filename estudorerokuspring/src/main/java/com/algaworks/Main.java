888c2ca06d7730b7177e9c8e1eff109e0376507d
Eduardo  Nofre <eduardo.sa@tcibpo.com>
2019-05-27 09:43:11 -0300

    ajustando o main.class

--- estudorerokuspring/src/main/java/com/algaworks/Main.java
+++ estudorerokuspring/src/main/java/com/algaworks/Main.java
@@ -5,7 +5,6 @@ package com.algaworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * @author eduardo.sa
